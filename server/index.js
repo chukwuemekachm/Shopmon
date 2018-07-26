bd48809625db37d33a5e00de4a3c7a2796462391
Chima Chukwuemeka <chukwuemekachm@gmail.com>
2018-07-26 20:18:26 +0100

    feat(server): Complete setup for base application
    - Install express
    - Install body-parser
    - Install cors
    - Install morgan
    - Install nodemon
    - Install babel-cli
    - Install babel-core
    - Install babel-preset-env
    - Setup base application
    - Setup base routes
    - Add package-lock.json to .gitignore
    
    [Finishes #159330874]

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('/api/v1', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome to Shopmon API version 1',
+  });
+});
+
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: 'Route unavailable on this server',
+  })
+});
+
+app.use((err, req, res) => {
+  res.status(err.statusCode || 500).json({
+    status: 'error',
+    message: err.statusMessage || 'Request unprocessable',
+  });
+});
+
+app.listen(PORT);
+
+export default app;
