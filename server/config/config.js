e5ca8c4cb84b13fad53d998761bcf57f5250f979
CHIMA CHUKWUEMEKA EMMANUEL <chukwuemekachm@gmail.com>
2018-07-30 00:33:12 +0100

    chore(server): completes sequelize and database setup
    - Install sequelize
    - Install pg
    - Install pg-hstore
    - Install sequelize-cli
    - Install dotenv
    - Setup sequelize
    - Add .env-sample file to show the environmental variables used
    - Add migrate and migrate:undo scripts to package.json
    
    [Finishes #159330993]

---
+++ server/config/config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_DATABASE || 'shopmon-dev',
+    port: process.env.DB_PORT || 5432,
+    host: process.env.DB_HOST || 'localhost',
+    dialect: process.env.DB_DIALECT || 'postgres',
+  },
+  test: {
+    use_env_variable: 'DATABASE_URL'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL'
+  }
+}
